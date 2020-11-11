00ccddd556dd87e64268a0c68fec4aa42ebf797a
Balwan Biradar <Balwan Biradar@DESKTOP-1KFO58I.clarion.local>
2020-11-11 16:21:21 +0530

    again inserted the code

---
+++ Newest/src/testcases/TC002.java
@@ -0,0 +1,22 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC002 {
+	
+	
+	@Test
+	public void testcase1()
+	{
+	System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+	ChromeDriver driver= new ChromeDriver();
+	driver.get("http://www.facebook.com");
+	driver.findElementById("email").sendKeys("balwan@clarion");
+	driver.findElementById("pass").sendKeys("123456");
+	;
+	
+	
+	}
+
+}
